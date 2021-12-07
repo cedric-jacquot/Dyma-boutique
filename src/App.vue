66e4b16cfbe213c9770a589a326ac10305b401e7
cedric-jacquot <cedric.jacquot54@gmail.com>
2021-12-07 23:34:48 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Nav/>
+    <Header/>
+    <Section/>
+    <Footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav from './components/Nav.vue';
+import Header from './components/Header.vue';
+import Section from './components/Section.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Nav,
+    Header,
+    Section,
+    Footer,
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Section.vue
@@ -0,0 +1,41 @@
+<template>
+        <!-- Section-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                
+                    <!-- ICI LES PRODUITS A LISTER -->
+                    <Produit/>
+                    <Produit/>
+                    <Produit/>
+                    <Produit/>
+                    <Produit/>
+                    <Produit/>
+                    <Produit/>
+                    <Produit/>
+                    <Produit/>
+                    <Produit/>
+                    <Produit/>
+                    <Produit/>
+                    <Produit/>
+                    <Produit/>
+                    <Produit/>
+                    <Produit/>
+
+                </div>
+            </div>
+        </section>
+</template>
+
+<script>
+import Produit from './Produit.vue';
+
+export default {
+  name: 'Section',
+  components: {
+      Produit,
+  },
+  props: {
+  }
+}
+</script>
