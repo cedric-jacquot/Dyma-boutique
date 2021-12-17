6199fa606532025490570281f5b6da67a97608cc
Cedric <cedric.jacquot54@gmail.com>
2021-12-17 10:36:19 +0100

    carte.length

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -2,7 +2,7 @@
   <!-- Section-->
   <section class="py-5">
     <div class="container px-4 px-lg-5 mt-5">
-      <pre>cart : {{ cart }}</pre>
+      <pre>cart : {{ cart.length }}</pre>
       <div
         class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center"
       >
